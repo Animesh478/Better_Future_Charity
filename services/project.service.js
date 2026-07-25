1d689a0128748555775940f0fcab4f953030eb1e
Animesh478 <animeshmohanty.am@gmail.com>
2026-07-25 15:34:09 +0530

    status column added to projects table

---
+++ services/project.service.js
@@ -0,0 +1,8 @@
+const createProjectInDb = async function (userId, projectDetails) {};
+
+const updateProjectInDb = async function () {};
+
+module.exports = {
+  createProjectInDb,
+  updateProjectInDb,
+};
